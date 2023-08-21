35920a531db2d53cff04e88a7a6ec677c6f8c431
EmersonLaranja <emersonlaranja@gmail.com>
2023-08-21 15:43:52 -0300

    2.2

--- src/controllers/PetController.ts
+++ src/controllers/PetController.ts
@@ -10,11 +10,49 @@ export default class PetController {
     if (!id || !adotado || !especie || !nome || !idade) {
       return res
         .status(400)
-        .json({ error: "Todos os campos são obrigatórios." });
+        .json({ erro: "Todos os campos são obrigatórios." });
     }
 
-    const novoPet: TipoPet = { id, idade, adotado, especie, nome };
+    const novoPet: TipoPet = { id: Number(id), idade, adotado, especie, nome };
     listaDePets.push(novoPet);
     return res.status(204).json(novoPet);
+  }
+
+  listaPets(req: Request, res: Response) {
+    return res.status(200).json(listaDePets);
+  }
+
+  buscaPetPorId(req: Request, res: Response) {
+    const { id } = req.params;
+    const pet = listaDePets.find((pet) => pet.id === Number(id));
+    if (!pet) {
+      return res.status(404).json({ erro: "Pet não encontrado." });
+    }
+    return res.status(200).json(pet);
+  }
+
+  atualizaPet(req: Request, res: Response) {
+    const { id } = req.params;
+    const { nome, idade, especie, adotado } = req.body as TipoPet;
+    const pet = listaDePets.find((pet) => pet.id === Number(id));
+    if (!pet) {
+      return res.status(404).json({ erro: "Pet não encontrado." });
+    }
+    pet.nome = nome;
+    pet.idade = idade;
+    pet.especie = especie;
+    pet.adotado = adotado;
+    return res.status(200).json(pet);
+  }
+
+  deletaPet(req: Request, res: Response) {
+    const { id } = req.params;
+    const pet = listaDePets.find((pet) => pet.id === Number(id));
+    if (!pet) {
+      return res.status(404).json({ erro: "Pet não encontrado." });
+    }
+    const indice = listaDePets.indexOf(pet);
+    listaDePets.splice(indice, 1);
+    return res.status(204).json();
   }
 }
--- src/routes/petRoutes.ts
+++ src/routes/petRoutes.ts
@@ -4,5 +4,9 @@ const router = express.Router();
 const petController = new PetController();
 
 router.post("/", (req, res) => petController.criaPet(req, res));
+router.get("/", (req, res) => petController.listaPets(req, res));
+router.get("/:id", (req, res) => petController.buscaPetPorId(req, res));
+router.put("/:id", (req, res) => petController.atualizaPet(req, res));
+router.delete("/:id", (req, res) => petController.deletaPet(req, res));
 
 export default router;
